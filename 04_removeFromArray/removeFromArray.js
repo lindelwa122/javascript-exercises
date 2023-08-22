567501e64bc6fed2219c937044890e484cd5a72c
Lindelwa <lindelwa.codes@gmail.com>
2023-08-22 17:41:25 +0200

    Complete exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,25 @@
-const removeFromArray = function() {
+/*
 
+PSEUDOCODE
+
+for each unwantedItem
+    find its index and create a variable called index of type number to store it
+    if index is zero or bigger then
+        remove an item at position index of arr
+
+return arr
+
+*/
+
+const removeFromArray = function (arr, ...unwantedItems) {
+  unwantedItems.forEach((unwantedItem) => {
+    const index = arr.findIndex((val) => val === unwantedItem);
+    if (index >= 0) {
+      arr.splice(index, 1);
+    }
+  });
+
+  return arr;
 };
 
 // Do not edit below this line
