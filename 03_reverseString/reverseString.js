fce8d87d45913831f04525f8b86a3e55274e3c3a
Lindelwa <lindelwa.codes@gmail.com>
2023-08-22 16:54:59 +0200

    Complete exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,16 @@
-const reverseString = function() {
+/* 
+    PSEUDOCODE
 
+    split the string to a list and create a variable called splittedString that stores the result
+    reverse splittedString
+    join splittedString using '' as a sperator and return the results
+
+*/
+
+const reverseString = function(word) {
+    const splittedString = word.split('')
+    splittedString.reverse();
+    return splittedString.join('');
 };
 
 // Do not edit below this line
