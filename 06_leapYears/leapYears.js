9b2d7efd02b9ceb9ea385a99922dda6168139ac1
Lindelwa <lindelwa.codes@gmail.com>
2023-08-22 18:54:28 +0200

    Complete exercise 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,29 @@
-const leapYears = function() {
+/*
 
+definition of a leapYear
+
+year divisible by 4
+
+HOWEVER,
+
+year divisible by 100 !== leapYear
+
+UNLESS,
+
+year divisible by 400
+
+*/
+
+const leapYears = function (year) {
+  if (year % 4 === 0) {
+    if (year % 100 === 0 && year % 400 === 0) {
+      return true;
+    } else if (year % 100 === 0) {
+      return false;
+    }
+
+    return true;
+  } else return false;
 };
 
 // Do not edit below this line
