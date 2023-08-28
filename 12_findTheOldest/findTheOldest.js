5dadec347f7288e3f57e2c3f585847f1a9617bde
Lindelwa <lindelwa.codes@gmail.com>
2023-08-28 21:34:29 +0200

    Complete exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
+const getAge = (person) => {
+    if (!person.yearOfDeath) {
+        const currentYear = (new Date).getFullYear();
+        return currentYear - person.yearOfBirth;
+    }
 
+    return person.yearOfDeath - person.yearOfBirth;
+}
+
+const findTheOldest = function(arr) {
+    const peopleAges = arr.map((person) => getAge(person));
+    const highestAge = Math.max(...peopleAges);
+    const index = peopleAges.findIndex((age) => age === highestAge);
+    return arr[index];
 };
 
 // Do not edit below this line
