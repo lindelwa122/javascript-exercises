ac811ebd9aaab3af935b69295ad7b27de77591f2
Lindelwa <lindelwa.codes@gmail.com>
2023-08-28 20:30:49 +0200

    Complete exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,25 @@
-const palindromes = function () {
+const reverseArrCopy = (arr) => {
+    // returns reversed copy without mutating the original
+    const newArr = [];
 
+    for (const item of arr) {
+        newArr.unshift(item);
+    }
+
+    return newArr;
+}
+
+const palindromes = function (string) {
+    const punctuations = ['.', ',', ' ', '!'];
+
+    const newString = string.split('').filter((char, index) => {
+        return !punctuations.includes(char);
+    });
+
+    const reversedString = reverseArrCopy(newString);
+
+    return newString.join('').toLowerCase() 
+        === reversedString.join('').toLowerCase();
 };
 
 // Do not edit below this line
